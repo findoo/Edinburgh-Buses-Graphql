d57267f829f750aba493fdc7c376c7be9f8b40f9
Finlay Smith <finlay.smith@me.com>
2018-12-21 20:35:08 +0000

    Adding buses and journeys

--- BusAPI.js
+++ BusAPI.js
@@ -1,19 +1,21 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 const crypto = require("crypto");
 const { format } = require("date-fns");
+const Papa = require("papaparse");
+const request = require("request");
+const zlib = require("zlib");
 
 const API_KEY = "xxxxxxxxxxx";
 
 class BusAPI extends RESTDataSource {
-
   constructor() {
     super();
-    this.baseURL = 'http://ws.mybustracker.co.uk/';
+    this.baseURL = "http://ws.mybustracker.co.uk/";
   }
 
   willSendRequest(request) {
-    request.params.set('module', 'json');
-    request.params.set('key', this.getEncryptedKey());
+    request.params.set("module", "json");
+    request.params.set("key", this.getEncryptedKey());
   }
 
   getEncryptedKey() {
@@ -22,6 +24,25 @@ class BusAPI extends RESTDataSource {
       .createHash("md5")
       .update(key)
       .digest("hex");
+  }
+
+  getGzipped(url) {
+    return new Promise((res, rej) => {
+      const headers = { "Accept-Encoding": "gzip" };
+      const buffer = [];
+      const gunzip = zlib.createGunzip();
+      request({ url, headers }).pipe(gunzip);
+      gunzip
+        .on("data", data => {
+          buffer.push(data.toString());
+        })
+        .on("finish", () => {
+          return res(buffer.join(""));
+        })
+        .on("error", e => {
+          return rej(e);
+        });
+    });
   }
 
   async getServices() {
@@ -37,6 +58,26 @@ class BusAPI extends RESTDataSource {
   async getBusStops() {
     const data = await this.get("?function=getBusStops");
     return data.busStops;
+  }
+
+  async getJourneyTimes(stopId, journeyId) {
+    if (stopId && journeyId) {
+      const data = await this.get(
+        `?function=getJourneyTimes&stopId=${stopId}&journeyId=${journeyId}`
+      );
+      return data.journeyTimes[0];
+    } else {
+      return null;
+    }
+  }
+
+  async getBuses() {
+    const csvData = await this.getGzipped(
+      `${
+        this.baseURL
+      }?module=csv&function=getVehicleLocations&key=${this.getEncryptedKey()}`
+    );
+    return Papa.parse(csvData, { header: true }).data;
   }
 }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const bngconvert = require("bngconvert");
 const BusAPI = require("./BusAPI");
 
 const typeDefs = gql`
@@ -6,6 +7,7 @@ const typeDefs = gql`
     getBusStops: [BusStop]
     getServices: [Service]
     getDests: [Destination]
+    getBuses: [Bus]
   }
 
   type Service {
@@ -24,18 +26,56 @@ const typeDefs = gql`
     service: Service
   }
 
+  type Journey {
+    journeyId: String
+    operatorId: String
+    service: Service
+    dest: Destination
+    journeyTimeDatas: [JourneyTime]
+    globalDisruption: Boolean
+    serviceDisruption: Boolean
+    serviceDiversion: Boolean
+  }
+
+  type JourneyTime {
+    order: Int
+    stop: BusStop
+    day: Int
+    minutes: Int
+    reliability: String
+    type: String
+    busStopDisruption: Boolean
+  }
+
+  type Bus {
+    busId: String
+    service: Service
+    dest: Destination
+    nextStop: BusStop
+    operatorId: String
+    lat: Float
+    lon: Float
+    speed: String
+    bearing: String
+    lastTimeKnow: String
+    journey: Journey
+  }
+
   type BusStop {
     ref: String
     operatorId: String
     stopId: String
     name: String
     lat: Float
-    long: Float
+    lon: Float
     cap: Int
     services: [Service]
     dests: [Destination]
   }
 `;
+
+const mapRefsToObjects = (refs, objects, matcher = "ref") =>
+  refs.map(r => objects.find(o => o[matcher] === r));
 
 const resolvers = {
   Query: {
@@ -43,44 +83,86 @@ const resolvers = {
       dataSources.busAPI.getServices(),
     getDests: async (_source, _args, { dataSources }) =>
       dataSources.busAPI.getDests(),
+    getBuses: async (_source, _args, { dataSources }) =>
+      dataSources.busAPI.getBuses(),
     getBusStops: async (_source, _args, { dataSources }) =>
       dataSources.busAPI.getBusStops()
   },
 
   BusStop: {
-    services: async (busStop, _args, { dataSources }) => {
-      const servs = await dataSources.busAPI.getServices();
-      return busStop.services.map(bs => servs.find(s => bs === s.ref));
-    },
-    dests: async (busStop, _args, { dataSources }) => {
-      const dests = await dataSources.busAPI.getDests();
-      return busStop.dests.map(bs => dests.find(s => bs === s.ref));
-    }
+    services: async (busStop, _args, { dataSources }) =>
+      mapRefsToObjects(
+        busStop.services,
+        await dataSources.busAPI.getServices()
+      ),
+    dests: async (busStop, _args, { dataSources }) =>
+      mapRefsToObjects(busStop.dests, await dataSources.busAPI.getDests()),
+    lat: busStop => bngconvert.OSGB36toWGS84(busStop.x, busStop.y)[0],
+    lon: busStop => bngconvert.OSGB36toWGS84(busStop.x, busStop.y)[1]
   },
 
   Destination: {
-    service: async (destination, _args, { dataSources }) => {
-      const servs = await dataSources.busAPI.getServices();
-      return servs.find(s => s.ref === destination.service);
-    }
+    service: async (destination, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [destination.service],
+        await dataSources.busAPI.getServices()
+      )[0]
   },
 
   Service: {
-    dests: async (service, _args, { dataSources }) => {
-      const dests = await dataSources.busAPI.getDests();
-      return service.dests.map(sd => dests.find(d => sd === d.ref));
-    }
+    dests: async (service, _args, { dataSources }) =>
+      mapRefsToObjects(service.dests, await dataSources.busAPI.getDests())
+  },
+
+  Bus: {
+    service: async (bus, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [bus.refService],
+        await dataSources.busAPI.getServices()
+      )[0],
+    dest: async (bus, _args, { dataSources }) =>
+      mapRefsToObjects([bus.refDest], await dataSources.busAPI.getDests())[0],
+    nextStop: async (bus, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [bus.nextStop],
+        await dataSources.busAPI.getBusStops(),
+        "stopId"
+      )[0],
+    lat: bus => bngconvert.OSGB36toWGS84(bus.x, bus.y)[0],
+    lon: bus => bngconvert.OSGB36toWGS84(bus.x, bus.y)[1],
+    journey: async (bus, _args, { dataSources }) =>
+      await dataSources.busAPI.getJourneyTimes(bus.nextStop, bus.journeyId)
+  },
+
+  Journey: {
+    dest: async (journey, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [journey.refDest],
+        await dataSources.busAPI.getDests()
+      )[0],
+    service: async (journey, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [journey.refService],
+        await dataSources.busAPI.getServices()
+      )[0]
+  },
+
+  JourneyTime: {
+    stop: async (journeyTime, _args, { dataSources }) =>
+      mapRefsToObjects(
+        [journeyTime.stopId],
+        await dataSources.busAPI.getBusStops(),
+        "stopId"
+      )[0]
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  dataSources: () => {
-    return {
-      busAPI: new BusAPI()
-    };
-  }
+  dataSources: () => ({
+    busAPI: new BusAPI()
+  })
 });
 
 server.listen().then(({ url }) => {
